0b12d70550854b8e396089a3d8ffca7ddf4253ae
Alan_gui <2516394328@qq.com>
2021-03-28 18:24:09 +0800

    nacos

---
+++ workstation-nacos/src/main/java/com/qing/niu/workstation/nacos/NacosProviderApplicationStart.java
@@ -0,0 +1,18 @@
+package com.qing.niu.workstation.nacos;
+
+/**
+ * <p>
+ * </p>
+ *
+ * @Author Alan_gui
+ * @Date 2021/3/28
+ * @ProjectName IntelliJ IDEA
+ * @Package com.qing.niu.workstation.nacos
+ * @Version 1.0.0
+ */
+public class NacosProviderApplicationStart {
+
+    public static void main(String[] args) {
+
+    }
+}
